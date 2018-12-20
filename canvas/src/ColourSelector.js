242954a78dadd274004b6e069c5b460772d92283
Santosh <santosh.poudyal@gmail.com>
2018-12-20 11:17:33 +1100

    set up ColourSelector and event in the colour picker

--- canvas/src/ColourSelector.js
+++ canvas/src/ColourSelector.js
@@ -1,12 +1,28 @@
 import React, {Component} from "react";
 
 class ColourSelector extends Component {
+    state = {hex: this.props.hex}
+    
+    // onInputChange(event) {
+    //     console.log(event.target.value);
+
+    // }
+
+    componentDidUpdate(){
+        console.log(this.state);
+    }
 
     render(){
         const {hex} = this.props;
         return(
             <div>
-                <input type="color" defaultValue={hex}/>
+                <input 
+                    type="color" 
+                    //defaultValue = "0000ff"
+                    value={hex} 
+                    // onChange={this.onInputChange}
+                    onChange={event => this.setState({hex: event.target.value})}
+                /> 
             </div>
         );
     }
